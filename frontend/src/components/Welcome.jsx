deworkerpro/demo-auction

Added data-test attribute

frontend/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import styles from './Welcome.module.css'
 
 function Welcome() {
   return (
-    <div className={styles.welcome}>
+    <div data-test="welcome" className={styles.welcome}>
       <h1>Auction</h1>
       <p>We will be here soon</p>
     </div>

dionisvl/php.demo-auction

frontend/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import styles from './Welcome.module.css'
 
 function Welcome() {
   return (
-    <div className={styles.welcome}>
+    <div data-testid="welcome" className={styles.welcome}>
       <h1>Auction</h1>
       <p>We will be here soon</p>
     </div>