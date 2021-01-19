deworkerpro/demo-auction

Added network attach use case

api/src/Auth/Command/AttachNetwork/Command.php
@@ -1,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Command\AttachNetwork;
+
+class Command
+{
+    public string $id = '';
+    public string $network = '';
+    public string $identity = '';
+}

api/src/Auth/Command/AttachNetwork/Handler.php
@@ -1,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Command\AttachNetwork;
+
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\NetworkIdentity;
+use App\Auth\Entity\User\UserRepository;
+use App\Flusher;
+
+class Handler
+{
+    private UserRepository $users;
+    private Flusher $flusher;
+
+    public function __construct(UserRepository $users, Flusher $flusher)
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $identity = new NetworkIdentity($command->network, $command->identity);
+
+        if ($this->users->hasByNetwork($identity)) {
+            throw new \DomainException('User with this network already exists.');
+        }
+
+        $user = $this->users->get(new Id($command->id));
+
+        $user->attachNetwork($identity);
+
+        $this->flusher->flush();
+    }
+}

dionisvl/php.demo-auction

api/src/Auth/Command/AttachNetwork/Command.php
@@ -1,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\AttachNetwork;
+
+class Command
+{
+    public string $id = '';
+    public string $network = '';
+    public string $identity = '';
+}

api/src/Auth/Command/AttachNetwork/Handler.php
@@ -1,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\AttachNetwork;
+
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\NetworkIdentity;
+use App\Auth\Entity\User\UserRepository;
+use App\Flusher;
+
+class Handler
+{
+    private UserRepository $users;
+    private Flusher $flusher;
+
+    public function __construct(UserRepository $users, Flusher $flusher)
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $identity = new NetworkIdentity($command->network, $command->identity);
+
+        if ($this->users->hasByNetwork($identity)) {
+            throw new \DomainException('User with this network already exists.');
+        }
+
+        $user = $this->users->get(new Id($command->id));
+
+        $user->attachNetwork($identity);
+
+        $this->flusher->flush();
+    }
+}