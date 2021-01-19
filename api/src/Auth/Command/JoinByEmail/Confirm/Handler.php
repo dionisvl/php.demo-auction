rwenglowski/demo-auction

Added join confirm use case

api/src/Auth/Command/JoinByEmail/Confirm/Handler.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\JoinByEmail\Confirm;
+
+use App\Auth\Entity\User\UserRepository;
+use App\Flusher;
+use DateTimeImmutable;
+use DomainException;
+
+class Handler
+{
+    private UserRepository $users;
+    private Flusher $flusher;
+
+    public function __construct(UserRepository $users, Flusher $flusher)
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        if (!$user = $this->users->findByConfirmToken($command->token)) {
+            throw new DomainException('Incorrect token.');
+        }
+
+        $user->confirmJoin($command->token, new DateTimeImmutable());
+
+        $this->flusher->flush();
+    }
+}

api/src/Auth/Entity/User/UserRepository.php
@@ -7,5 +7,6 @@ namespace App\Auth\Entity\User;
 interface UserRepository
 {
     public function hasByEmail(Email $email): bool;
+    public function findByConfirmToken(string $token): ?User;
     public function add(User $user): void;
 }

dionisvl/php.demo-auction

api/src/Auth/Command/JoinByEmail/Confirm/Handler.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\JoinByEmail\Confirm;
+
+use App\Auth\Entity\User\UserRepository;
+use App\Flusher;
+use DateTimeImmutable;
+use DomainException;
+
+class Handler
+{
+    private UserRepository $users;
+    private Flusher $flusher;
+
+    public function __construct(UserRepository $users, Flusher $flusher)
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        if (!$user = $this->users->findByJoinConfirmToken($command->token)) {
+            throw new DomainException('Incorrect token.');
+        }
+
+        $user->confirmJoin($command->token, new DateTimeImmutable());
+
+        $this->flusher->flush();
+    }
+}

api/src/Auth/Entity/User/UserRepository.php
@@ -7,5 +7,6 @@ namespace App\Auth\Entity\User;
 interface UserRepository
 {
     public function hasByEmail(Email $email): bool;
+    public function findByJoinConfirmToken(string $token): ?User;
     public function add(User $user): void;
 }