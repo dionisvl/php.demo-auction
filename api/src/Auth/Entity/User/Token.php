deworkerpro/demo-auction

Added embedded nullables

api/src/Auth/Entity/User/Token.php
@@ -61,4 +61,9 @@ class Token
     {
         return $this->expires;
     }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->token);
+    }
 }

dionisvl/php.demo-auction

api/src/Auth/Entity/User/Token.php
@@ -61,4 +61,9 @@ class Token
     {
         return $this->expires;
     }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->value);
+    }
 }