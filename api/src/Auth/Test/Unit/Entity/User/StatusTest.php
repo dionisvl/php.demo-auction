deworkerpro/demo-auction

Added status public constructor

api/src/Auth/Test/Unit/Entity/User/StatusTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Auth\Test\Unit\Entity\User;
 
 use App\Auth\Entity\User\Status;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,6 +13,19 @@ use PHPUnit\Framework\TestCase;
  */
 class StatusTest extends TestCase
 {
+    public function testSuccess(): void
+    {
+        $role = new Status($name = Status::WAIT);
+
+        self::assertEquals($name, $role->getName());
+    }
+
+    public function testIncorrect(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Status('none');
+    }
+
     public function testWait(): void
     {
         $status = Status::wait();

dionisvl/php.demo-auction

api/src/Auth/Test/Unit/Entity/User/StatusTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Auth\Test\Unit\Entity\User;
 
 use App\Auth\Entity\User\Status;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,6 +13,19 @@ use PHPUnit\Framework\TestCase;
  */
 class StatusTest extends TestCase
 {
+    public function testSuccess(): void
+    {
+        $status = new Status($name = Status::WAIT);
+
+        self::assertEquals($name, $status->getName());
+    }
+
+    public function testIncorrect(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Status('none');
+    }
+
     public function testWait(): void
     {
         $status = Status::wait();