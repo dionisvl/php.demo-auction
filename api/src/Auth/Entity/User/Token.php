d10795c03d7af68f1780ecfa006f52cbe0b6c3b5
dionisvl <dden_vlad@mail.ru>
2022-02-17 17:08:33 +0300

    21_Mapping сущностей на таблицы в БД

--- api/src/Auth/Entity/User/Token.php
+++ api/src/Auth/Entity/User/Token.php
@@ -8,10 +8,21 @@ use DateTimeImmutable;
 use DomainException;
 use Webmozart\Assert\Assert;
 
+/**
+ * @ORM\Embeddable
+ */
 class Token
 {
-    private string $value;
-    private DateTimeImmutable $expires;
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $value;
+    /**
+     * @var DateTimeImmutable
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $expires;
 
     public function __construct(string $value, DateTimeImmutable $expires)
     {
@@ -48,5 +59,10 @@ class Token
     public function getExpires(): DateTimeImmutable
     {
         return $this->expires;
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->value);
     }
 }
