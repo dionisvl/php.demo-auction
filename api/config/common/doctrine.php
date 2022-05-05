e40515936ff3331686071c811036b590353410da
den <dden_vlad@mail.ru>
2022-05-05 21:56:08 +0300

    54 Обновление Doctrine и Jenkins

--- api/config/common/doctrine.php
+++ api/config/common/doctrine.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 use App\Auth;
-use Doctrine\Common\Cache\ArrayCache;
-use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\Common\Cache\Psr6\DoctrineProvider;
 use Doctrine\Common\EventManager;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\DBAL\Types\Type;
@@ -13,6 +12,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 use Doctrine\ORM\Tools\Setup;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use function App\env;
 
 return [
     EntityManagerInterface::class => static function (ContainerInterface $container): EntityManagerInterface {
@@ -30,14 +32,14 @@ return [
          */
         $settings = $container->get('config')['doctrine'];
 
-        /**
-         * @psalm-suppress DeprecatedClass
-         */
+        /** @psalm-suppress DeprecatedClass */
         $config = Setup::createAnnotationMetadataConfiguration(
             $settings['metadata_dirs'],
             $settings['dev_mode'],
             $settings['proxy_dir'],
-            $settings['cache_dir'] ? new FilesystemCache($settings['cache_dir']) : new ArrayCache(),
+            $settings['cache_dir'] ?
+                DoctrineProvider::wrap(new FilesystemAdapter('', 0, $settings['cache_dir'])) :
+                DoctrineProvider::wrap(new ArrayAdapter()),
             false
         );
 
@@ -71,10 +73,10 @@ return [
             'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',
             'connection' => [
                 'driver' => 'pdo_pgsql',
-                'host' => getenv('DB_HOST'),
-                'user' => getenv('DB_USER'),
-                'password' => getenv('DB_PASSWORD'),
-                'dbname' => getenv('DB_NAME'),
+                'host' => env('DB_HOST'),
+                'user' => env('DB_USER'),
+                'password' => env('DB_PASSWORD'),
+                'dbname' => env('DB_NAME'),
                 'charset' => 'utf-8',
             ],
             'subscribers' => [],
