be88c03e2db731bc6d95c673fbdc15a703bc0cf9
dionisvl <dden_vlad@mail.ru>
2022-05-28 20:29:55 +0300

    63 React Router 6 и Атрибуты PHP 8

--- api/config/common/validator.php
+++ api/config/common/validator.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -10,9 +9,6 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 return [
     ValidatorInterface::class => static function (ContainerInterface $container): ValidatorInterface {
-        /** @psalm-suppress DeprecatedMethod */
-        AnnotationRegistry::registerLoader('class_exists');
-
         $translator = $container->get(TranslatorInterface::class);
 
         return Validation::createValidatorBuilder()
